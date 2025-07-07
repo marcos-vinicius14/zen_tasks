c0cb2d6e162812ec9561f1867c9f3a391d2c02a5
marcosvinicius <marcos.vinicius.dev3@gmail.com>
2025-07-07 20:41:00 -0300

    feat: create TaskModel

--- zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/tasks/model/TaskModel.java
+++ zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/tasks/model/TaskModel.java
@@ -1,4 +1,134 @@
 package com.marcos.dev.zentasks.zen_task_api.tasks.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_tasks")
 public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 150)
+    private String title;
+
+    @Column(name = "description", length = 500, nullable = false)
+    private String description;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "is_urgent")
+    private boolean isUrgent;
+
+    @Column(name = "is_important")
+    private boolean isImportant;
+
+    @Column(name = "is_completed")
+    private boolean isCompleted;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    public TaskModel() {
+    }
+
+    public TaskModel(String title, String description, LocalDate dueDate, boolean isUrgent, boolean isImportant, boolean isCompleted, LocalDateTime completedAt) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isUrgent = isUrgent;
+        this.isImportant = isImportant;
+        this.isCompleted = isCompleted;
+        this.completedAt = completedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isUrgent() {
+        return isUrgent;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setUrgent(boolean urgent) {
+        isUrgent = urgent;
+    }
+
+    public void setImportant(boolean important) {
+        isImportant = important;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskModel taskModel = (TaskModel) o;
+        return isUrgent == taskModel.isUrgent && isImportant == taskModel.isImportant && isCompleted == taskModel.isCompleted && Objects.equals(id, taskModel.id) && Objects.equals(title, taskModel.title) && Objects.equals(description, taskModel.description) && Objects.equals(dueDate, taskModel.dueDate) && Objects.equals(createdAt, taskModel.createdAt) && Objects.equals(completedAt, taskModel.completedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, dueDate, isUrgent, isImportant, isCompleted, createdAt, completedAt);
+    }
 }
