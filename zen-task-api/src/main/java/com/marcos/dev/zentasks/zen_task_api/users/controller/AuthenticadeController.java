9ea9bdf80883226c1760701cb6239f0df4080e00
marcosvinicius <marcos.vinicius.dev3@gmail.com>
2025-08-10 10:05:09 -0300

    feat: create login

---
+++ zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/users/controller/AuthenticadeController.java
@@ -0,0 +1,31 @@
+package com.marcos.dev.zentasks.zen_task_api.users.controller;
+
+import com.marcos.dev.zentasks.zen_task_api.users.dtos.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticadeController {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticadeController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity login(
+           @RequestBody
+           @Valid AuthenticationDTO data
+    ) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+
+        Authentication authenticate = this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        return ResponseEntity.ok().build();
+    }
+}
--- zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/users/infra/security/SecurityConfiguration.java
+++ zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/users/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.marcos.dev.zentasks.zen_task_api.users.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,7 +15,17 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Para Swagger
+                        .anyRequest().authenticated()
+                )
+                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
 }
