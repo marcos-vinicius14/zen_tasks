251badcecd700466974daa9b783e7054be9ec673
marcos <marcos.vinicius.dev3@gmail.com>
2025-08-30 19:56:16 -0300

    refactor: error hadler is more robust

--- zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/tasks/dtos/ErrorResponseDTO.java
+++ zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/tasks/dtos/ErrorResponseDTO.java
@@ -2,10 +2,32 @@ package com.marcos.dev.zentasks.zen_task_api.tasks.dtos;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponseDTO(
-        int status,
-        String error,
-        String message,
-        LocalDateTime timestamp
-) {
+public class ErrorResponseDTO {
+    private final int statusCode;
+    private final String message;
+    private final String details;
+    private final LocalDateTime timestamp;
+
+    public ErrorResponseDTO(int statusCode, String message, String details, LocalDateTime timestamp) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.details = details;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
---
+++ zen-task-api/src/main/java/com/marcos/dev/zentasks/zen_task_api/tasks/dtos/ValidationErrorResponseDTO.java
@@ -0,0 +1,18 @@
+package com.marcos.dev.zentasks.zen_task_api.tasks.dtos;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class ValidationErrorResponseDTO extends ErrorResponseDTO {
+
+    private final Map<String, String> errors;
+
+    public ValidationErrorResponseDTO(int statusCode, String message, String details, LocalDateTime timestamp, Map<String, String> errors) {
+        super(statusCode, message, details, timestamp);
+        this.errors = errors;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+}
