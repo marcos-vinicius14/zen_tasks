8ff96e9e4eb7cf7ee79f93c9f2c651b0ff11b376
marcos <marcos.vinicius.dev3@gmail.com>
2025-09-28 15:37:50 -0300

    feat: remove localstorage to sotrage token

--- zen-task-front/src/services/api/auth.ts
+++ zen-task-front/src/services/api/auth.ts
@@ -14,4 +14,12 @@ export const authApi = {
   register: async (userData: RegisterRequest): Promise<AuthResponse> => {
     return apiClient.post<AuthResponse>(API_ENDPOINTS.AUTH.REGISTER, userData, { authRequired: false });
   },
+
+  logout: async():  Promise<void> => {
+    return apiClient.post(API_ENDPOINTS.AUTH.LOGOUT);
+  },
+
+  status: async(): Promise<void> => {
+    return apiClient.get(API_ENDPOINTS.AUTH.STATUS)
+  }
 };
